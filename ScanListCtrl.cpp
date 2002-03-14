8c5d857033f07a64309ef1ca2fb0632e15578d16
angryziber <angryziber>
2002-03-14 11:46:16 +0000

    new class for list control

---
+++ ScanListCtrl.cpp
@@ -0,0 +1,33 @@
+// ScanListCtrl.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "ipscan.h"
+#include "ScanListCtrl.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// CScanListCtrl
+
+CScanListCtrl::CScanListCtrl()
+{
+}
+
+CScanListCtrl::~CScanListCtrl()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CScanListCtrl, CListCtrl)
+	//{{AFX_MSG_MAP(CScanListCtrl)
+		// NOTE - the ClassWizard will add and remove mapping macros here.
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// CScanListCtrl message handlers
---
+++ ScanListCtrl.h
@@ -0,0 +1,48 @@
+#if !defined(AFX_SCANLISTCTRL_H__204B478B_DE3A_42DE_8A5B_F1665F484EAA__INCLUDED_)
+#define AFX_SCANLISTCTRL_H__204B478B_DE3A_42DE_8A5B_F1665F484EAA__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+// ScanListCtrl.h : header file
+//
+
+/////////////////////////////////////////////////////////////////////////////
+// CScanListCtrl window
+
+class CScanListCtrl : public CListCtrl
+{
+// Construction
+public:
+	CScanListCtrl();
+
+// Attributes
+public:
+
+// Operations
+public:
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CScanListCtrl)
+	//}}AFX_VIRTUAL
+
+// Implementation
+public:
+	virtual ~CScanListCtrl();
+
+	// Generated message map functions
+protected:
+	//{{AFX_MSG(CScanListCtrl)
+		// NOTE - the ClassWizard will add and remove member functions here.
+	//}}AFX_MSG
+
+	DECLARE_MESSAGE_MAP()
+};
+
+/////////////////////////////////////////////////////////////////////////////
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_SCANLISTCTRL_H__204B478B_DE3A_42DE_8A5B_F1665F484EAA__INCLUDED_)
