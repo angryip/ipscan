8cce65cb0e3e057f1f57be31c77f21c0a7e8bdd7
angryziber <angryziber>
2004-07-20 14:34:03 +0000

    new class

---
+++ ipfeeder.cpp
@@ -0,0 +1,34 @@
+/*********************************************************************
+ * This is a part of Angry IP IPFeeder source code                    *
+ * http://www.angryziber.com/ipscan/                                 *
+ *                                                                   *
+ * Written by Angryziber                                             *
+ *                                                                   *
+ * You may distribute this code as long as this message is not       *
+ * removed and it is clear who has written it.                       *
+ * You may not rename the program and distribute it.                 *
+ *********************************************************************/
+
+// IPFeeder.cpp: implementation of the CIPFeeder class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "stdafx.h"
+#include "ipscan.h"
+#include "IPFeeder.h"
+#include "ScanUtilsInternal.h"
+#include "ScanUtilsPlugin.h"
+#include "IpscanDlg.h"
+#include "globals.h"
+
+#ifdef _DEBUG
+#undef THIS_FILE
+static char THIS_FILE[]=__FILE__;
+#define new DEBUG_NEW
+#endif
+
+
+
+
+
+
---
+++ ipfeeder.h
@@ -0,0 +1,33 @@
+/*********************************************************************
+ * This is a part of Angry IP IPFeeder source code                    *
+ * http://www.angryziber.com/ipscan/                                 *
+ *                                                                   *
+ * Written by Angryziber                                             *
+ *                                                                   *
+ * You may distribute this code as long as this message is not       *
+ * removed and it is clear who has written it.                       *
+ * You may not rename the program and distribute it.                 *
+ *********************************************************************/
+
+// IPFeeder.h: interface for the CIPFeeder class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_IPFeeder_H__F6305E28_F29C_45F5_8073_26591A6C68D1__INCLUDED_)
+#define AFX_IPFeeder_H__F6305E28_F29C_45F5_8073_26591A6C68D1__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include <afxtempl.h>	// For CArray
+
+#include "plugin.h"		// Structures and defines for plugins
+
+class CIPFeeder  
+{
+public:	
+protected:		
+};
+
+#endif // !defined(AFX_IPFeeder_H__F6305E28_F29C_45F5_8073_26591A6C68D1__INCLUDED_)
