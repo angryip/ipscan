a72c6c81573cdf84eca567022ec11e2e8e816376
angryziber <angryziber@375186e5-ef17-0410-b0b6-91563547dcda>
2006-08-29 21:34:43 +0000

    GPL text added
    git-svn-id: https://ipscan.svn.sourceforge.net/svnroot/ipscan/ipscan@19 375186e5-ef17-0410-b0b6-91563547dcda

--- src/net/azib/ipscan/gui/AboutWindow.java
+++ src/net/azib/ipscan/gui/AboutWindow.java
@@ -62,11 +62,23 @@ public class AboutWindow extends AbstractModalDialog {
 		textLabel.addListener(SWT.Selection, new HelpActions.Website());
 		textLabel.pack();
 		
-		Text licenseText = new Text(shell, SWT.BORDER | SWT.MULTI | SWT.READ_ONLY | SWT.V_SCROLL);
+		Text licenseText = new Text(shell, SWT.BORDER | SWT.MULTI | SWT.READ_ONLY | SWT.V_SCROLL | SWT.WRAP);
 		licenseText.setBounds(60, 140, 320, 160);
 		licenseText.setBackground(shell.getDisplay().getSystemColor(SWT.COLOR_LIST_BACKGROUND));
 		// TODO: load full GPL text from file
-		licenseText.setText("This program is licensed under GPL\n\nFull GPL text will be here");
+		licenseText.setText("Licensed under the GNU General Public License Version 2\n\n" +
+							Version.NAME + " is free software; you can redistribute it and/or " +
+							"modify it under the terms of the GNU General Public License " +
+							"as published by the Free Software Foundation; either version 2 " +
+							"of the License, or (at your option) any later version.\n\n" +
+							Version.NAME + " is distributed in the hope that it will be useful, " +
+							"but WITHOUT ANY WARRANTY; without even the implied warranty of " +
+							"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the " +
+							"GNU General Public License for more details.\n\n" +
+							"You should have received a copy of the GNU General Public License " +
+							"along with this program; if not, write to the Free Software " +
+							"Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA " +
+							"02110-1301, USA, or visit http://www.fsf.org/");
 		
 		Button button = new Button(shell, SWT.NONE);
 		button.setText(Labels.getInstance().getString("button.close"));
