873b547ff0d2874b0a6c97a7b13b09f6930b2edb
angryziber <angryziber@375186e5-ef17-0410-b0b6-91563547dcda>
2007-01-04 14:13:36 +0000

    UTF-8 and \n are now used by default for all files. File templates are now also project-specific, so should work for anyone, who does a checkout. Comments at the top of most files have been fixed.
    git-svn-id: https://ipscan.svn.sourceforge.net/svnroot/ipscan/trunk@114 375186e5-ef17-0410-b0b6-91563547dcda

--- ipscan/src/net/azib/ipscan/core/ScanningSubject.java
+++ ipscan/src/net/azib/ipscan/core/ScanningSubject.java
@@ -1,8 +1,10 @@
 /**
  * This file is a part of Angry IP Scanner source code,
  * see http://www.azib.net/ for more information.
+ * Licensed under GPLv2.
  */
 package net.azib.ipscan.core;
+
 
 import java.net.InetAddress;
 import java.util.HashMap;
--- ipscan/src/net/azib/ipscan/core/net/PingResult.java
+++ ipscan/src/net/azib/ipscan/core/net/PingResult.java
@@ -1,5 +1,7 @@
 /**
- * 
+ * This file is a part of Angry IP Scanner source code,
+ * see http://www.azib.net/ for more information.
+ * Licensed under GPLv2.
  */
 package net.azib.ipscan.core.net;
 
