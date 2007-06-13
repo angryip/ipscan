753b6cdde92624999ca1f6ffa2c464636b189b63
angryziber <angryziber@375186e5-ef17-0410-b0b6-91563547dcda>
2007-06-13 21:39:33 +0000

    temporarily moved ipscan to the upper level
    git-svn-id: https://ipscan.svn.sourceforge.net/svnroot/ipscan/trunk@128 375186e5-ef17-0410-b0b6-91563547dcda

--- ipscan/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Note: on Mac, the -XstartOnFirstThread JVM options is mandatory, -Xdock:name="Angry IP Scanner" is optional!
-
-echo "Executing built Angry IP Scanner under root"
-su -c 'java -Djava.library.path=ext/rocksaw/lib -jar dist/ipscan.jar'
