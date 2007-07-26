ef9f3ce0139bf36496c37c443bc9034e342a2c86
angryziber <angryziber@375186e5-ef17-0410-b0b6-91563547dcda>
2007-07-26 17:56:40 +0000

    bugfix: fetcher names provided to exporters were empty
    git-svn-id: https://ipscan.svn.sourceforge.net/svnroot/ipscan/trunk@190 375186e5-ef17-0410-b0b6-91563547dcda

--- src/net/azib/ipscan/exporters/ExportProcessor.java
+++ src/net/azib/ipscan/exporters/ExportProcessor.java
@@ -45,7 +45,7 @@ public class ExportProcessor {
 			String[] fetcherNames = new String[fetchers.size()];
 			int i = 0;
 			for (Fetcher fetcher : fetchers) {
-				fetcherNames[i] = Labels.getLabel(fetcher.getLabel());
+				fetcherNames[i++] = Labels.getLabel(fetcher.getLabel());
 			}			
 			exporter.setFetchers(fetcherNames);
 
