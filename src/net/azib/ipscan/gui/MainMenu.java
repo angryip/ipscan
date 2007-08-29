75f9b54dc95d1c6b8cd02864b352b9df0346195f
angryziber <angryziber@375186e5-ef17-0410-b0b6-91563547dcda>
2007-08-29 17:34:18 +0000

    hiding unimplemented menus
    git-svn-id: https://ipscan.svn.sourceforge.net/svnroot/ipscan/releases/3.0-alpha2@217 375186e5-ef17-0410-b0b6-91563547dcda

--- src/net/azib/ipscan/gui/MainMenu.java
+++ src/net/azib/ipscan/gui/MainMenu.java
@@ -67,9 +67,9 @@ public class MainMenu {
 //		initMenuItem(subMenu, null, null, null, null);
 		initMenuItem(subMenu, "menu.file.saveAll", "Ctrl+S", new Integer(SWT.MOD1 | 'S'), initListener(FileActions.SaveAll.class));
 		initMenuItem(subMenu, "menu.file.saveSelection", null, null, initListener(FileActions.SaveSelection.class));
-		initMenuItem(subMenu, null, null, null, null);
-		initMenuItem(subMenu, "menu.file.exportPreferences", null, null, null);
-		initMenuItem(subMenu, "menu.file.importPreferences", null, null, null);
+//		initMenuItem(subMenu, null, null, null, null);
+//		initMenuItem(subMenu, "menu.file.exportPreferences", null, null, null);
+//		initMenuItem(subMenu, "menu.file.importPreferences", null, null, null);
 		if (!Platform.MAC_OS) {
 			initMenuItem(subMenu, null, null, null, null);
 			initMenuItem(subMenu, "menu.file.exit", !Platform.MAC_OS ? "Alt+F4" : null, null, initListener(FileActions.Exit.class));
@@ -91,7 +91,7 @@ public class MainMenu {
 		initMenuItem(subMenu, "menu.tools.preferences", "Ctrl+O", new Integer(SWT.MOD1 | (Platform.MAC_OS ? ',' : 'O')), initListener(ToolsActions.Preferences.class));
 		initMenuItem(subMenu, "menu.tools.fetchers", "Ctrl+Shift+O", new Integer(SWT.MOD1 | SWT.MOD2 | (Platform.MAC_OS ? ',' : 'O')), initListener(ToolsActions.SelectFetchers.class));
 		initMenuItem(subMenu, null, null, null, null);
-		initMenuItem(subMenu, "menu.tools.delete", null, null, null);
+//		initMenuItem(subMenu, "menu.tools.delete", null, null, null);
 		initMenuItem(subMenu, "menu.tools.scanInfo", "Ctrl+I", new Integer(SWT.MOD1 | 'I'), initListener(ToolsActions.ScanInfo.class));
 		
 		subMenu = initMenu(menu, "menu.help");
@@ -101,8 +101,8 @@ public class MainMenu {
 		initMenuItem(subMenu, "menu.help.forum", null, null, initListener(HelpActions.Forum.class));
 		initMenuItem(subMenu, "menu.help.plugins", null, null, initListener(HelpActions.Plugins.class));
 		initMenuItem(subMenu, null, null, null, null);
-		initMenuItem(subMenu, "menu.help.cmdLine", null, null, null);
-		initMenuItem(subMenu, null, null, null, null);
+//		initMenuItem(subMenu, "menu.help.cmdLine", null, null, null);
+//		initMenuItem(subMenu, null, null, null, null);
 		initMenuItem(subMenu, "menu.help.checkVersion", null, null, initListener(HelpActions.CheckVersion.class));
 		initMenuItem(subMenu, null, null, null, null);
 		initMenuItem(subMenu, "menu.help.about", null, null, initListener(HelpActions.About.class));
