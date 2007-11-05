e997c9f9d4062335cdfbcafe59e412be5bff501e
angryziber <angryziber@375186e5-ef17-0410-b0b6-91563547dcda>
2007-11-05 20:47:17 +0000

    Removed dependencies from Link class (and therefore from all accessibility classes and native libs) - AboutDialog now handles the link itself using a Label
    git-svn-id: https://ipscan.svn.sourceforge.net/svnroot/ipscan/trunk@228 375186e5-ef17-0410-b0b6-91563547dcda

--- src/net/azib/ipscan/gui/AboutDialog.java
+++ src/net/azib/ipscan/gui/AboutDialog.java
@@ -5,14 +5,16 @@ package net.azib.ipscan.gui;
 
 import net.azib.ipscan.config.Labels;
 import net.azib.ipscan.config.Version;
-import net.azib.ipscan.gui.actions.HelpActions;
+import net.azib.ipscan.gui.actions.BrowserLauncher;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Link;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
@@ -47,26 +49,42 @@ public class AboutDialog extends AbstractModalDialog {
 		iconLabel.pack();
 		int leftBound = iconLabel.getBounds().width + 20;
 
-		Link textLabel = new Link(shell, SWT.NONE);
-		String text = Labels.getLabel("text.about");
-		text = text.replaceAll("%NAME", Version.NAME);
-		text = text.replaceAll("%VERSION", Version.getVersion());
-		text = text.replaceAll("%BUILD", Version.getBuildNumber());
-		text = text.replaceAll("%DATE", Version.getBuildDate());
-		text = text.replaceAll("%JAVA", System.getProperty("java.vm.vendor") + " " + System.getProperty("java.vm.version"));
-		text = text.replaceAll("%OS", System.getProperty("os.name") + " " + System.getProperty("os.version"));
-		text = text.replaceAll("%COPYLEFT", Version.COPYLEFT);
-		text = text.replaceAll("%WEBSITE", Version.WEBSITE);
-		text = text.replaceAll("%MAILTO", Version.MAILTO);
-		textLabel.setText(text);
-		textLabel.setLocation(leftBound, 10);
-		textLabel.addListener(SWT.Selection, new HelpActions.Website());
-		textLabel.pack();
+		String aboutText = Labels.getLabel("text.about");
+		aboutText = aboutText.replaceAll("%NAME", Version.NAME);
+		aboutText = aboutText.replaceAll("%VERSION", Version.getVersion());
+		aboutText = aboutText.replaceAll("%BUILD", Version.getBuildNumber());
+		aboutText = aboutText.replaceAll("%DATE", Version.getBuildDate());
+		aboutText = aboutText.replaceAll("%COPYLEFT", Version.COPYLEFT);
+		Label aboutLabel = new Label(shell, SWT.NONE);
+		aboutLabel.setText(aboutText);
+		aboutLabel.setLocation(leftBound, 10);
+		aboutLabel.pack();
+		
+		final Label websiteLabel = new Label(shell, SWT.NONE);
+		websiteLabel.setForeground(new Color(null, 0, 0, 0xCC));
+		websiteLabel.setCursor(shell.getDisplay().getSystemCursor(SWT.CURSOR_HAND));
+		websiteLabel.setText(Version.WEBSITE);
+		websiteLabel.setLocation(leftBound, 10 + aboutLabel.getBounds().height);
+		websiteLabel.addListener(SWT.MouseUp, new Listener() {
+			public void handleEvent(Event event) {
+				BrowserLauncher.openURL(Version.WEBSITE);
+				websiteLabel.setForeground(new Color(null, 0x88, 0, 0xAA));
+			}
+		});
+		websiteLabel.pack();
+
+		String systemText = Labels.getLabel("text.about.system");
+		systemText = systemText.replaceAll("%JAVA", System.getProperty("java.vm.vendor") + " " + System.getProperty("java.vm.version"));
+		systemText = systemText.replaceAll("%OS", System.getProperty("os.name") + " " + System.getProperty("os.version"));
+		Label systemLabel = new Label(shell, SWT.NONE);
+		systemLabel.setText(systemText);
+		systemLabel.setLocation(leftBound, 20 + aboutLabel.getBounds().height + websiteLabel.getBounds().height);
+		systemLabel.pack();
 		
 		Button button = createCloseButton();
 		
 		Text licenseText = new Text(shell, SWT.BORDER | SWT.MULTI | SWT.READ_ONLY | SWT.V_SCROLL | SWT.WRAP);
-		licenseText.setBounds(leftBound, textLabel.getBounds().y+textLabel.getBounds().height+10, shell.getClientArea().width - leftBound - 10, button.getLocation().y - 174);
+		licenseText.setBounds(leftBound, systemLabel.getBounds().y + systemLabel.getBounds().height + 10, shell.getClientArea().width - leftBound - 10, button.getLocation().y - 174);
 		licenseText.setBackground(shell.getDisplay().getSystemColor(SWT.COLOR_LIST_BACKGROUND));
 		licenseText.setText("Licensed under the GNU General Public License Version 2\n\n" +
 							Version.NAME + " is free software; you can redistribute it and/or " +
