b78f5a071eb5a11cd54ac88b6da6ee4d32a9863c
angryziber <angryziber@375186e5-ef17-0410-b0b6-91563547dcda>
2007-11-10 17:15:46 +0000

    More default openers: * Ping * Traceroute * Geo locate (via Web)
    git-svn-id: https://ipscan.svn.sourceforge.net/svnroot/ipscan/trunk@240 375186e5-ef17-0410-b0b6-91563547dcda

--- src/net/azib/ipscan/config/OpenersConfig.java
+++ src/net/azib/ipscan/config/OpenersConfig.java
@@ -28,7 +28,10 @@ public class OpenersConfig extends NamedListConfig {
 			add(labels.get("opener.web"), new Opener("http://${fetcher.ip}/", false, null));			
 			add(labels.get("opener.ftp"), new Opener("ftp://${fetcher.ip}/", false, null));
 			add(labels.get("opener.telnet"), new Opener("telnet ${fetcher.ip}", true, null));
+			add(labels.get("opener.ping"), new Opener("ping ${fetcher.ip}", true, null));
+			add(labels.get("opener.traceroute"), new Opener((Platform.WINDOWS ? "tracert" : Platform.LINUX ? "/usr/sbin/tracepath" : "traceroute") + " ${fetcher.ip}", true, null));
 			if (!Platform.WINDOWS) add(labels.get("opener.ssh"), new Opener("ssh ${fetcher.ip}", true, null));
+			add(labels.get("opener.geolocate"), new Opener("http://www.azib.net/iplocate.php?ip=${fetcher.ip}", false, null));
 			add(labels.get("opener.email"), new Opener("mailto:somebody@example.com?subject=IP: ${fetcher.ip}", true, null));
 		}
 	}
