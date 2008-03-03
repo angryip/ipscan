558916aa2301111dc1ba8d8cacac26fb2d266a6b
angryziber <angryziber@375186e5-ef17-0410-b0b6-91563547dcda>
2008-03-03 20:54:14 +0000

    version checking is now done in a separate thread to keep main window responsive
    git-svn-id: https://ipscan.svn.sourceforge.net/svnroot/ipscan/trunk@269 375186e5-ef17-0410-b0b6-91563547dcda

--- src/net/azib/ipscan/gui/actions/HelpActions.java
+++ src/net/azib/ipscan/gui/actions/HelpActions.java
@@ -10,6 +10,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import net.azib.ipscan.config.Labels;
 import net.azib.ipscan.config.Version;
@@ -19,6 +21,7 @@ import net.azib.ipscan.gui.GettingStartedDialog;
 import net.azib.ipscan.gui.StatusBar;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.MessageBox;
@@ -29,7 +32,6 @@ import org.eclipse.swt.widgets.MessageBox;
  * @author Anton Keks
  */
 public class HelpActions {
-	
 	public static final class GettingStarted implements Listener {
 		public void handleEvent(Event event) {
 			new GettingStartedDialog().open();
@@ -67,46 +69,58 @@ public class HelpActions {
 			this.statusBar = statusBar;
 		}
 
-		public void handleEvent(Event event) {
-			BufferedReader reader = null;
-			try {
-				statusBar.setStatusText(Labels.getLabel("state.retrievingVersion"));
-				
-				URL url = new URL(Version.LATEST_VERSION_URL);
-				URLConnection conn = url.openConnection();
-				reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-				
-				String latestVersion = reader.readLine();
-				latestVersion = latestVersion.substring(latestVersion.indexOf(' ')+1);
-				
-				reader.close();
-				
-				MessageBox messageBox = new MessageBox(event.display.getActiveShell(), SWT.ICON_INFORMATION);
-				messageBox.setText(Version.getFullName());
-				
-				if (!Version.getVersion().equals(latestVersion)) {
-					String message = Labels.getLabel("text.version.old");
-					message = message.replaceFirst("%LATEST", latestVersion);
-					message = message.replaceFirst("%VERSION", Version.getVersion());
-					messageBox.setMessage(message);
+		public void handleEvent(final Event event) {
+			statusBar.setStatusText(Labels.getLabel("state.retrievingVersion"));
+			
+			// prepare message box in advance
+			final MessageBox messageBox = new MessageBox(event.display.getActiveShell(), SWT.ICON_INFORMATION);
+			messageBox.setText(Version.getFullName());
+
+			Runnable checkVersionCode = new Runnable() {
+				public void run() {
+					BufferedReader reader = null;
+					String message = null;
+					try {
+						URL url = new URL(Version.LATEST_VERSION_URL);
+						URLConnection conn = url.openConnection();
+						reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+						
+						String latestVersion = reader.readLine();
+						latestVersion = latestVersion.substring(latestVersion.indexOf(' ')+1);
+						
+						if (!Version.getVersion().equals(latestVersion)) {
+							message = Labels.getLabel("text.version.old");
+							message = message.replaceFirst("%LATEST", latestVersion);
+							message = message.replaceFirst("%VERSION", Version.getVersion());
+						}
+						else {
+							message = Labels.getLabel("text.version.latest");
+						}
+					}
+					catch (Exception e) {
+						message = Labels.getLabel("exception.UserErrorException.version.latestFailed");
+						Logger.getLogger(getClass().getName()).log(Level.WARNING, message, e);
+					}
+					finally {
+						try {
+							if (reader != null)
+								reader.close();
+						}
+						catch (IOException e) {}
+						
+						// show the box in the SWT thread
+						final String messageToShow = message;
+						Display.getDefault().asyncExec(new Runnable() {
+							public void run() {
+								statusBar.setStatusText(null);
+								messageBox.setMessage(messageToShow);
+								messageBox.open();
+							}
+						});
+					}
 				}
-				else {
-					messageBox.setMessage(Labels.getLabel("text.version.latest"));
-				}
-				messageBox.open();
-			}
-			catch (Exception e) {
-				throw new UserErrorException("version.latestFailed", e);
-			}
-			finally {
-				try {
-					if (reader != null)
-						reader.close();
-				}
-				catch (IOException e) {}
-				
-				statusBar.setStatusText(null);
-			}
+			};
+			new Thread(checkVersionCode).start();
 		}
 	}
 }
