0de388a00a5cb4735bc9327b33190daaa571747d
Anton Keks <anton@codeborne.com>
2014-03-12 21:59:17 +0200

    updated mac vendors as well as URL of wireshark source

--- update-mac-vendors.sh
+++ update-mac-vendors.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This scripts downloads and optimizes Wireshark's MAC vendor database
 
-curl http://anonsvn.wireshark.org/wireshark/trunk/manuf |\
+curl 'https://code.wireshark.org/review/gitweb?p=wireshark.git;a=blob_plain;f=manuf;hb=HEAD' |\
 grep -P '^[0-9A-F:]{8}\t' | awk '{print $1 $2}' | sed 's/://g' \
 > resources/mac-vendors.txt
 
