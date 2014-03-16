8bcceac825c52b28d512e59e6a4c7561a0f02008
Anton Keks <anton@codeborne.com>
2014-03-16 17:27:10 +0200

    read Windows XP to Windows Vista as crippled, but Windows 7 as not - tcp connection rate limiting was removed in Vista SP2

--- src/net/azib/ipscan/config/Platform.java
+++ src/net/azib/ipscan/config/Platform.java
@@ -5,6 +5,8 @@
  */
 package net.azib.ipscan.config;
 
+import static java.lang.Double.parseDouble;
+
 /**
  * This class provides constants for distinguishing between various platforms.
  * However, platform-specific behaviour must be kept at minimum.
@@ -27,8 +29,12 @@ public class Platform {
 	public static final boolean WINDOWS = OS_NAME.startsWith("Windows");
 	
 	/** Crippled-down version of Windows (no RawSockets, TCP rate limiting, etc */
-	public static final boolean CRIPPLED_WINDOWS = WINDOWS && !OS_NAME.contains("Server") && Double.parseDouble(System.getProperty("os.version").substring(0, 3)) >= 5.1;
+	public static final boolean CRIPPLED_WINDOWS = WINDOWS && !OS_NAME.contains("Server") && between(parseDouble(System.getProperty("os.version").substring(0, 3)), 5.1, 6.1);
 	
 	/** GNU Java, based on GIJ/GCC and GNU Classpath projects */
 	public static final boolean GNU_JAVA = System.getProperty("java.vm.vendor").contains("Free Software Foundation");
+
+	private static boolean between(double x, double min, double max) {
+		return x >= min && x < max;
+	}
 }
