96a241b55606861f3805b83f340211b56b63e024
Anton Keks <anton@azib.net>
2014-09-30 22:38:41 +0300

    do not use sed, do replace with awk

--- update-mac-vendors.sh
+++ update-mac-vendors.sh
@@ -2,7 +2,7 @@
 # This scripts downloads and optimizes Wireshark's MAC vendor database
 
 curl 'https://code.wireshark.org/review/gitweb?p=wireshark.git;a=blob_plain;f=manuf;hb=HEAD' |\
-grep -P '^[0-9A-F:]{8}\t' | awk '{print $1 $2}' | sed 's/://g' \
+grep -P '^[0-9A-F:]{8}\t' | awk '{gsub(":","",$1); print $1 $2}' \
 > resources/mac-vendors.txt
 
 wc -l resources/mac-vendors.txt
