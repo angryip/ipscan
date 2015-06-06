402bb194d9d5f8c6ab533e5117624edc6b8ede8b
Andriy Kryvtsun <kontiky@gmail.com>
2015-06-06 16:01:07 -0400

    extract Exporters

--- src/net/azib/ipscan/MainEntrance.java
+++ src/net/azib/ipscan/MainEntrance.java
@@ -4,11 +4,16 @@ import dagger.Component;
 import net.azib.ipscan.config.ComponentRegistry;
 import net.azib.ipscan.config.ConfigModule;
 import net.azib.ipscan.core.PluginLoader;
+import net.azib.ipscan.exporters.ExporterModule;
 
 /**
  * Created by englishman on 6/6/15.
  */
-@Component(modules = {ConfigModule.class, PluginLoader.class})
+@Component(modules = {
+	ConfigModule.class,
+	PluginLoader.class,
+	ExporterModule.class
+})
 public interface MainEntrance {
 	ComponentRegistry getRegistry();
 }
