33fde722d9758942198208c17ac33031fe1ee553
englishman <kontiky@gmail.com>
2015-08-05 17:06:46 -0400

    add FetcherModule

--- src/net/azib/ipscan/MainComponent.java
+++ src/net/azib/ipscan/MainComponent.java
@@ -5,6 +5,7 @@ import net.azib.ipscan.config.ComponentRegistry;
 import net.azib.ipscan.config.ConfigModule;
 import net.azib.ipscan.core.PluginLoader;
 import net.azib.ipscan.exporters.ExporterModule;
+import net.azib.ipscan.fetchers.FetcherModule;
 
 /**
  * Created by englishman on 6/6/15.
@@ -12,7 +13,8 @@ import net.azib.ipscan.exporters.ExporterModule;
 @Component(modules = {
 	ConfigModule.class,
 	PluginLoader.class,
-	ExporterModule.class
+	ExporterModule.class,
+	FetcherModule.class
 })
 public interface MainComponent {
 	ComponentRegistry get();
