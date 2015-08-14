60296a22ffd842b92912542d6ec09f88c650c477
englishman <kontiky@gmail.com>
2015-08-14 11:39:45 -0400

    makes application startable

--- src/net/azib/ipscan/config/MainComponent.java
+++ src/net/azib/ipscan/config/MainComponent.java
@@ -5,12 +5,15 @@ import net.azib.ipscan.core.PluginLoader;
 import net.azib.ipscan.gui.MacApplicationMenu;
 import net.azib.ipscan.gui.MainWindow;
 
+import javax.inject.Singleton;
+
 @Component(modules = {
 		Config.class,
 		Labels.class,
 		PluginLoader.class,
 		ComponentRegistry.class
 })
+@Singleton
 public interface MainComponent {
 	MainWindow getMainWindow();
 	MacApplicationMenu getMacApplicationMenu();
