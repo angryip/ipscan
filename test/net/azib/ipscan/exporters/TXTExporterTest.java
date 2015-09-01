d518ea685e25dcfbef7932fe676a3d723c2dafe3
Anton Keks <anton@codeborne.com>
2015-09-02 00:32:14 +0300

    add basic test for importing of results

--- test/net/azib/ipscan/exporters/TXTExporterTest.java
+++ test/net/azib/ipscan/exporters/TXTExporterTest.java
@@ -3,13 +3,17 @@
  */
 package net.azib.ipscan.exporters;
 
-import static org.junit.Assert.*;
-
-import java.io.IOException;
-
+import net.azib.ipscan.config.Version;
+import net.azib.ipscan.core.ScanningResult;
+import net.azib.ipscan.gui.feeders.AbstractFeederGUI;
 import org.junit.Test;
 
-import net.azib.ipscan.config.Version;
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
 
 /**
  * TXT Exporter Test
@@ -66,5 +70,14 @@ public class TXTExporterTest extends AbstractExporterTestCase {
 		exporter.end();
 		assertContains("192.168.1.1 - 192.168.3.255");
 	}
-	
+
+	@Test
+	public void importFromFile() throws Exception {
+		String file = getClass().getResource("import.txt").getPath();
+		AbstractFeederGUI feederGUI = mock(AbstractFeederGUI.class);
+
+		List<ScanningResult> results = ((TXTExporter) exporter).importResults(file, feederGUI);
+
+		assertEquals(7, results.size());
+	}
 }
