f59065bbe4793c4db10e629718928ffb5dcbfe7c
Anton Keks <anton@azib.net>
2016-12-11 23:27:51 +0200

    #47 remove also Incorporated suffix

--- update-mac-vendors.sh
+++ update-mac-vendors.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # This scripts downloads and optimizes IEEE MAC vendor list
 
-curl 'http://standards-oui.ieee.org/oui.txt' |\
-#cat oui.txt |\
+#curl 'http://standards-oui.ieee.org/oui.txt' |\
+cat oui.txt |\
 fgrep '(base 16)' | sed -r '
 	s/\r//g; s/     \(base 16\)\t\t//;
 	s/,? ?(Inc)\.?$//I;
-	s/(,|, | )(Ltd|Limited|GmbH|LLC|A\/S|AB|AS|SAS|AG|KG|PLC|SRL|OY|Oy|BV)\.?$//I;
-	s/(,|, | )(Co|Corp|Corporation|COMPANY)\.?$//I;
+	s/(,|, | )(Ltd|Limited|GmbH|LLC|A\/S|AB|AS|SAS|AG|KG|PLC|SRL|OY|Oy|BV|Nederland BV)\.?$//I;
+	s/(,|, | )(Co|Corp|Corporation|Company|Incorporated)\.?$//I;
 	s/\(.+\)//;
 	s/ (Electronics?|Technology|Technologies|Telecommunication|Communications?|Corporation|Systems|Solutions) ?//Ig
 	s/ Registration Authority//
