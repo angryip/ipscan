19b837dedb094a2902324bd790f48f745e3f9a37
Anton Keks <anton@azib.net>
2016-12-11 22:46:48 +0200

    #47 download mac vendors from IEEE, not truncating names to 8 chars anymore. This will increase binary size by 150kb

--- update-mac-vendors.sh
+++ update-mac-vendors.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-# This scripts downloads and optimizes Wireshark's MAC vendor database
+# This scripts downloads and optimizes IEEE MAC vendor list
 
-curl 'https://code.wireshark.org/review/gitweb?p=wireshark.git;a=blob_plain;f=manuf;hb=HEAD' |\
-grep -P '^[0-9A-F:]{8}\t' | awk '{gsub(":","",$1); print $1 $2}' \
+curl 'http://standards-oui.ieee.org/oui.txt' |\
+#cat oui.txt |\
+fgrep '(base 16)' | sed -r '
+	s/\r//g; s/     \(base 16\)\t\t//;
+	s/,? ?(Inc|INC|inc)\.?$//;
+	s/,? ?(Ltd|LTD|ltd|Limited|LIMITED|GmbH|GMBH|LLC|A\/S|AB|AS|SAS|AG|KG|plc|PLC|SRL|srl|OY|Oy)\.?$//;
+	s/,? ?(Co|CO|CORP|Corp|co|Corporation|CORPORATION|COMPANY)\.?$//;
+	s/\(.+\)//;
+	s/ ?(Electronics?|ELECTRONICS?|Technology|technology|TECHNOLOGY|Technologies|TECHNOLOGIES|Telecommunication|TELECOMMUNICATION|COMMUNICATION|Communication|Communications|Corporation|CORPORATION|corporation|Systems|Solutions) ?//g
+	s/ Registration Authority//
+' | sort \
 > resources/mac-vendors.txt
 
 wc -l resources/mac-vendors.txt
