75a041a218752992530573004220b040627b72d9
Anton Keks <anton@azib.net>
2016-12-11 23:31:55 +0200

    cut vendors names that are still too long

--- update-mac-vendors.sh
+++ update-mac-vendors.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # This scripts downloads and optimizes IEEE MAC vendor list
 
-#curl 'http://standards-oui.ieee.org/oui.txt' |\
-cat oui.txt |\
+curl 'http://standards-oui.ieee.org/oui.txt' |\
+#cat oui.txt |\
 fgrep '(base 16)' | sed -r '
 	s/\r//g; s/     \(base 16\)\t\t//;
 	s/,? ?(Inc)\.?$//I;
@@ -11,7 +11,7 @@ fgrep '(base 16)' | sed -r '
 	s/\(.+\)//;
 	s/ (Electronics?|Technology|Technologies|Telecommunication|Communications?|Corporation|Systems|Solutions) ?//Ig
 	s/ Registration Authority//
-' | sort \
+' | cut -c -42 | sort \
 > resources/mac-vendors.txt
 
 wc -l resources/mac-vendors.txt
