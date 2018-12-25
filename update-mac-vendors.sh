416ec7535a4f10b85e6ad04bbe72efca07e7c824
Anton Keks <anton@azib.net>
2018-12-25 15:59:18 +0200

    update mac vendors

--- update-mac-vendors.sh
+++ update-mac-vendors.sh
@@ -6,10 +6,10 @@ curl 'http://standards-oui.ieee.org/oui.txt' |\
 fgrep '(base 16)' | sed -r '
 	s/\r//g; s/     \(base 16\)\t\t//;
 	s/,? ?(Inc)\.?$//I;
-	s/(,|, | )(Ltd|Limited|GmbH|LLC|A\/S|AB|AS|SAS|AG|KG|PLC|SRL|OY|Oy|BV|Nederland BV)\.?$//I;
-	s/(,|, | )(Co|Corp|Corporation|Company|Incorporated)\.?$//I;
+	s/(,|, | )(Ltd|Limited|GmbH|LLC|A\/S|AB|AS|SAS|AG|KG|PLC|SRL|OY|Oy|BV|Nederland BV)\.?$//Ig;
+	s/(,|, | )(Co|Corp|Corporation|Company|Incorporated)\.?$//Ig;
 	s/\(.+\)//;
-	s/ (Electronics?|Technology|Technologies|Telecommunication|Communications?|Corporation|Systems|Solutions) ?//Ig
+	s/ (Electronics?|Technology|Technologies|Telecommunication|Communications?|Corporation|Systems|Solutions|International|Industry|Industries|Networks?|Holdings?) ?//Ig
 	s/ Registration Authority//
 ' | cut -c -42 | sort \
 > resources/mac-vendors.txt
