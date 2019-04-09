56e6df90de2cc41b1e1fcf21c7cfc27f904fb313
Anton Keks <anton@azib.net>
2019-04-09 22:54:44 +0300

    #129 add TODO to figure out how to fetch MAC address of a local IPv6 address

--- src/net/azib/ipscan/fetchers/WinMACFetcher.java
+++ src/net/azib/ipscan/fetchers/WinMACFetcher.java
@@ -4,6 +4,7 @@ import com.sun.jna.Memory;
 import com.sun.jna.Pointer;
 
 import javax.inject.Inject;
+import java.net.Inet4Address;
 import java.net.InetAddress;
 
 import static net.azib.ipscan.core.net.WinIpHlp.toIpAddr;
@@ -13,6 +14,8 @@ public class WinMACFetcher extends MACFetcher {
 	@Inject public WinMACFetcher() {}
 
 	@Override public String resolveMAC(InetAddress address) {
+		if (!(address instanceof Inet4Address)) return null; // TODO IPv6 support
+
 		Pointer pmac = new Memory(8);
 		Pointer plen = new Memory(4);
 		plen.setInt(0, 8);
