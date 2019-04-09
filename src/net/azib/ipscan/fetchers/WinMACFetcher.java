42736eb51535d22486d0e89dd78f518e0ab8fa4e
Anton Keks <anton@azib.net>
2019-04-09 22:13:21 +0300

    \#129 first working IPv6 pinging implementation for Windows

--- src/net/azib/ipscan/fetchers/WinMACFetcher.java
+++ src/net/azib/ipscan/fetchers/WinMACFetcher.java
@@ -2,25 +2,22 @@ package net.azib.ipscan.fetchers;
 
 import com.sun.jna.Memory;
 import com.sun.jna.Pointer;
-import net.azib.ipscan.core.net.WinIpHlpDll.IpAddrByVal;
 
 import javax.inject.Inject;
 import java.net.InetAddress;
 
+import static net.azib.ipscan.core.net.WinIpHlp.toIpAddr;
 import static net.azib.ipscan.core.net.WinIpHlpDll.dll;
 
 public class WinMACFetcher extends MACFetcher {
 	@Inject public WinMACFetcher() {}
 
 	@Override public String resolveMAC(InetAddress address) {
-		IpAddrByVal destIP = new IpAddrByVal();
-		destIP.bytes = address.getAddress();
-
 		Pointer pmac = new Memory(8);
 		Pointer plen = new Memory(4);
 		plen.setInt(0, 8);
 
-		int result = dll.SendARP(destIP, 0, pmac, plen);
+		int result = dll.SendARP(toIpAddr(address), 0, pmac, plen);
 
 		if (result != 0) return null;
 
