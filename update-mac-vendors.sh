5d86ba39a644894961685f51ac52a9e6ba92ddba
Anton Keks <anton@azib.net>
2019-08-11 20:24:50 +0300

    optimize mac vendor names a bit

--- update-mac-vendors.sh
+++ update-mac-vendors.sh
@@ -4,13 +4,14 @@
 curl 'http://standards-oui.ieee.org/oui.txt' |\
 #cat oui.txt |\
 fgrep '(base 16)' | sed -r '
-	s/\r//g; s/     \(base 16\)\t\t//;
-	s/,? ?(Inc)\.?$//I;
-	s/(,|, | )(Ltd|Limited|GmbH|LLC|A\/S|AB|AS|SAS|AG|KG|PLC|SRL|OY|Oy|BV|Nederland BV)\.?$//Ig;
-	s/(,|, | )(Co|Corp|Corporation|Company|Incorporated)\.?$//Ig;
-	s/\(.+\)//;
+	s/\r//g; s/     \(base 16\)\t\t//
+	s/,? ?(Inc)\.?$//I
+	s/(,|, | )(Ltd|Limited|GmbH|LLC|A\/S|AB|AS|SAS|AG|KG|PLC|SRL|OY|Oy|BV|Nederland BV|SAN VE TIC)\.?$//Ig
+	s/(,|, | )(Co|Corp|Corporation|Company|Incorporated)\.?$//Ig
+	s/\(.+\)//
 	s/ (Electronics?|Technology|Technologies|Telecommunication|Communications?|Corporation|Systems|Solutions|International|Industry|Industries|Networks?|Holdings?) ?//Ig
-	s/ Registration Authority//
+	s/ (Registration Authority| MobilityCommunication)//
+	s/SAMSUNG ELECTRO[- ]MECHANICS/Samsung/
 ' | cut -c -42 | sort \
 > resources/mac-vendors.txt
 
