f28218f08cc7875ea87552219c3e079770e51120
Anton Keks <anton@azib.net>
2019-10-15 00:25:16 +0300

    32-bit Oracle JRE doesn't report it's 32-bit, so assume 32-bit if we see "Client VM" (not Server VM of OpenJDK)

--- ext/win-installer/Installer/detectjvm.cmd
+++ ext/win-installer/Installer/detectjvm.cmd
@@ -1,4 +1,4 @@
-:: @echo off
+@echo off
 set JAVA_BIN="%JAVA_HOME%\bin\java.exe"
 
 if not exist %JAVA_BIN% set JAVA_BIN="java.exe"
@@ -6,5 +6,5 @@ if not exist %JAVA_BIN% set JAVA_BIN="java.exe"
 %JAVA_BIN% -version 2>&1 | findstr /I "64-bit"
 if %errorlevel% == 0 exit /b 64
 
-%JAVA_BIN% -version 2>&1 | findstr /I "32-bit"
+%JAVA_BIN% -version 2>&1 | findstr /I "32-bit|Client VM"
 if %errorlevel% == 0 exit /b 32
