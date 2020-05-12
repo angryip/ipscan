5b009b2a6003c8cddf3d6145a45d16736ca436b0
Anton Keks <anton@azib.net>
2020-05-12 13:57:12 -0700

    on Mac, JavaPinger produces many ConnectionExceptions, let's ignore them

--- src/net/azib/ipscan/core/net/JavaPinger.java
+++ src/net/azib/ipscan/core/net/JavaPinger.java
@@ -3,6 +3,7 @@ package net.azib.ipscan.core.net;
 import net.azib.ipscan.core.ScanningSubject;
 
 import java.io.IOException;
+import java.net.ConnectException;
 
 public class JavaPinger implements Pinger {
 	private int timeout;
@@ -15,9 +16,14 @@ public class JavaPinger implements Pinger {
 	public PingResult ping(ScanningSubject subject, int count) throws IOException {
 		PingResult result = new PingResult(subject.getAddress(), count);
 		for (int i = 0; i < count; i++) {
-			long t = System.currentTimeMillis();
-			if (subject.getAddress().isReachable(timeout))
-				result.addReply(System.currentTimeMillis() - t);
+			try {
+				long t = System.currentTimeMillis();
+				if (subject.getAddress().isReachable(timeout))
+					result.addReply(System.currentTimeMillis() - t);
+			}
+			catch (ConnectException e) {
+				// these happen on Mac
+			}
 		}
 		return result;
 	}
