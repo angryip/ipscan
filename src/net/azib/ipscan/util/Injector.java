40a807e47682a9a1bbe8d49a0ad83fa13c1e264b
Anton Keks <anton@azib.net>
2020-05-13 22:45:44 +0300

    add support for @Named dependencies

--- src/net/azib/ipscan/util/Injector.java
+++ src/net/azib/ipscan/util/Injector.java
@@ -1,31 +1,75 @@
 package net.azib.ipscan.util;
 
 import javax.inject.Inject;
+import javax.inject.Named;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 import static java.util.Arrays.stream;
+import static java.util.stream.IntStream.range;
 
+@SuppressWarnings("unchecked")
 public class Injector {
-	private final Map<Class<?>, Object> instances = new HashMap<>();
+	private final Map<Key<?>, Object> instances = new HashMap<>();
 
-	@SuppressWarnings("unchecked")
-	public <T> T inject(Class<T> type) {
-		return (T) instances.computeIfAbsent(type, this::createInstance);
+	public <T> void registerNamed(Class<T> type, String name, T impl) {
+		instances.put(new Key<>(type, name), impl);
 	}
 
-	@SuppressWarnings("unchecked")
+	public <T> T inject(Key<T> key) {
+		return (T) instances.computeIfAbsent(key, k -> createInstance(key.type));
+	}
+
+	public <T> T inject(Class<T> type) {
+		return inject(new Key<>(type, null));
+	}
+
 	private <T> T createInstance(Class<T> type) {
-		Constructor<?> constructor = stream(type.getConstructors()).filter(c -> c.isAnnotationPresent(Inject.class))
-				.findAny().orElseThrow(() -> new InjectException(type.getName() + " has no constructors annotated with @Inject"));
-		Object[] deps = stream(constructor.getParameterTypes()).map(this::inject).toArray();
+		Constructor<T> constructor = (Constructor<T>) stream(type.getConstructors())
+			.filter(c -> c.isAnnotationPresent(Inject.class)).findAny()
+			.orElseThrow(() -> new InjectException(type.getName() + " has no constructors annotated with @Inject"));
+		Object[] deps = depsKeys(constructor).map(this::inject).toArray();
 		try {
-			return (T) constructor.newInstance(deps);
+			return constructor.newInstance(deps);
 		}
 		catch (Exception e) {
-			throw new InjectException("Cannot create " + type.getName() + ", depending on " + Arrays.toString(deps), e);
+			throw new InjectException("Cannot create " + type.getName() + ", deps: " + Arrays.toString(deps), e);
+		}
+	}
+
+	private <T> Stream<Key<T>> depsKeys(Constructor<T> constructor) {
+		Class<?>[] types = constructor.getParameterTypes();
+		Annotation[][] ans = constructor.getParameterAnnotations();
+		return range(0, types.length).mapToObj(i -> new Key<>((Class<T>) types[i], findName(ans[i])));
+	}
+
+	private String findName(Annotation[] ans) {
+		return stream(ans).filter(a -> a.annotationType() == Named.class).findAny().map(a -> ((Named) a).value()).orElse(null);
+	}
+
+	public static class Key<T> {
+		final Class<T> type;
+		final String name;
+
+		public Key(Class<T> type, String name) {
+			this.type = type;
+			this.name = name;
+		}
+
+		@Override public boolean equals(Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+			Key<?> key = (Key<?>) o;
+			return type.equals(key.type) && Objects.equals(name, key.name);
+		}
+
+		@Override public int hashCode() {
+			return Objects.hash(type, name);
 		}
 	}
 
--- test/net/azib/ipscan/util/InjectorTest.java
+++ test/net/azib/ipscan/util/InjectorTest.java
@@ -3,7 +3,9 @@ package net.azib.ipscan.util;
 import org.junit.Test;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class InjectorTest {
@@ -15,11 +17,24 @@ public class InjectorTest {
 		assertTrue(injector.inject(WithDeps.class) instanceof WithDeps);
 	}
 
+	@Test
+	public void injectWithNamed() {
+		injector.registerNamed(String.class, "name", "mega-name");
+		assertEquals("mega-name", injector.inject(WithNamedDeps.class).name);
+	}
+
 	static class Dummy {
 		@Inject public Dummy() {}
 	}
 
 	static class WithDeps {
 		@Inject public WithDeps(Dummy dummy) {}
+	}
+
+	static class WithNamedDeps {
+		String name;
+		@Inject public WithNamedDeps(Dummy dummy, @Named("name") String name) {
+			this.name = name;
+		}
 	}
 }
