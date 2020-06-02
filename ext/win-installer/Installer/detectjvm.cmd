273e6e1e2853b58fed469bc6ba9b6ebb1f1b1516
Anton Keks <anton@codeborne.com>
2020-06-02 23:35:47 +0300

    #245 if detectjvm.cmd fails, then show a more descriptive error message and install 64-bit binary

--- ext/win-installer/Installer/detectjvm.cmd
+++ ext/win-installer/Installer/detectjvm.cmd
@@ -1,7 +1,7 @@
 @echo off
 set JAVA_BIN="%JAVA_HOME%\bin\java.exe"
 
-if not exist %JAVA_BIN% set JAVA_BIN="java.exe"
+if not exist %JAVA_BIN% set JAVA_BIN="java"
 
 %JAVA_BIN% -version 2>&1 | findstr /I "64-bit"
 if %errorlevel% == 0 exit /b 64
