2a470daf09e5516d061f87d6b816b430a4bc7147
Anton Keks <anton@codeborne.com>
2021-02-03 17:52:01 +0200

    align netmask also on Mac

--- src/net/azib/ipscan/gui/feeders/RangeFeederGUI.java
+++ src/net/azib/ipscan/gui/feeders/RangeFeederGUI.java
@@ -117,6 +117,7 @@ public class RangeFeederGUI extends AbstractFeederGUI {
 		Rectangle endIPBounds = endIPText.getBounds();
 		int width = endIPBounds.x + endIPBounds.width - comboBounds.x - 5;
 		if (Platform.WINDOWS) width -= 22; // TODO: remove width of down arrow, this number may change with updated SWT version
+		if (Platform.MAC_OS) width += 10;
 		((GridData) netmaskCombo.getLayoutData()).widthHint = width;
 		pack();
 
