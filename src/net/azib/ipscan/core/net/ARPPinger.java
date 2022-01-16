8d0251dbaacb7f3a796cd19220624642c00745d2
Anton Keks <anton@codeborne.com>
2022-01-16 23:23:18 +0200

    ARPPingerTest fails on mac, let's see if JavaPinger will work better as a trigger

--- src/net/azib/ipscan/core/net/ARPPinger.java
+++ src/net/azib/ipscan/core/net/ARPPinger.java
@@ -9,19 +9,19 @@ import static java.lang.System.currentTimeMillis;
 
 public class ARPPinger implements Pinger {
 	private MACFetcher macFetcher;
-	private UDPPinger udpPinger;
+	private JavaPinger trigger;
 
-	public ARPPinger(MACFetcher macFetcher, UDPPinger udpPinger) {
+	public ARPPinger(MACFetcher macFetcher, JavaPinger trigger) {
 		this.macFetcher = macFetcher;
 		// WinMACFetcher sends an actual ARP request, so no previous UDP request is needed
-		this.udpPinger = macFetcher.getClass().getSimpleName().startsWith("Win") ? null : udpPinger;
+		this.trigger = macFetcher.getClass().getSimpleName().startsWith("Win") ? null : trigger;
 	}
 
 	@Override public PingResult ping(ScanningSubject subject, int count) throws IOException {
 		PingResult result = new PingResult(subject.getAddress(), 1);
 		for (int i = 0; i < count; i++) {
 			long start = currentTimeMillis();
-			if (udpPinger != null) udpPinger.ping(subject, 1); // this should issue an ARP request for the IP
+			if (trigger != null) trigger.ping(subject, 1); // this should issue an ARP request for the IP
 			String mac = macFetcher.scan(subject);
 			if (mac != null) result.addReply(currentTimeMillis() - start);
 		}
