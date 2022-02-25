fee1741dfe8a724a7530dae83d94bada809daa87
Francesco Ambrosini <45211144+HellAmbro@users.noreply.github.com>
2022-02-25 01:15:48 +0100

    Export scanning results in SQL format

--- src/net/azib/ipscan/exporters/SQLExporter.java
+++ src/net/azib/ipscan/exporters/SQLExporter.java
@@ -1,6 +1,7 @@
 package net.azib.ipscan.exporters;
 
 import java.io.IOException;
+import java.io.OutputStream;
 
 /**
  * SQL Exporter
@@ -14,7 +15,9 @@ import java.io.IOException;
 public class SQLExporter extends AbstractExporter {
 
 	static final char DELIMETER = ':';
-	
+	static final String TABLE_NAME = "scan";
+	static final char COMMA = ',';
+
 	public String getId() {
 		return "exporter.sql";
 	}
@@ -22,11 +25,37 @@ public class SQLExporter extends AbstractExporter {
 	public String getFilenameExtension() {
 		return "sql";
 	}
-	
+
+	public void start(OutputStream outputStream, String feederInfo) throws IOException {
+		super.start(outputStream, feederInfo);
+
+		if (!append) {
+			output.println("DROP TABLE IF EXISTS scan;");
+		}
+	}
+
 	public void setFetchers(String[] fetcherNames) throws IOException {
+		if (!append) {
+			output.write("CREATE TABLE " + TABLE_NAME + " (`" + fetcherNames[0] + "` varchar(20)");
+			for (int i = 1; i < fetcherNames.length; i++) {
+				output.write(COMMA);
+				output.print(" `" + fetcherNames[i] + "` ");
+				output.write("varchar(20)"); //Default type
+			}
+			output.println(");");
+		}
 	}
-	
+
 	public void nextAddressResults(Object[] results) throws IOException {
-	
+		output.print("INSERT INTO " + TABLE_NAME + " VALUES ('" + results[0]+"'");
+		for (int i = 1; i < results.length; i++) {
+			Object result = results[i];
+			output.print(COMMA);
+			output.print(" ");
+			output.print("'" + result + "'");
+		}
+		output.println(");");
 	}
+
+
 }
